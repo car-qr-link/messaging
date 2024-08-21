375169c62eaf2d2c45f15f3cbb7039bb9ab1e97a
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-08-21 17:59:55 +0700

    [gateway] prepare gateway class

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,14 @@ export interface Config {
     BROKER_URL: string
     SEND_QUEUE: string
     RECEIVED_QUEUE: string
+
+    GATEWAY_URL: string
 }
 
 export const config: Config = {
     BROKER_URL: process.env.BROKER_URL || 'redis://localhost:6379/0',
     SEND_QUEUE: process.env.SEND_QUEUE || 'messages:send:sms',
     RECEIVED_QUEUE: process.env.RECEIVED_QUEUE || 'messages:received',
+
+    GATEWAY_URL: process.env.GATEWAY_URL || 'https://sms.capcom.me/api/3rdparty/v1',
 }
