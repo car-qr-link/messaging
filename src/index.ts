65980c93f6961013a140fc9f6493fc0cfc1b2895
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-08-22 16:58:23 +0700

    [services] move sending code into service

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,27 @@
-import { NotificationChannel, SendMessage, SendMessageSchema } from "@car-qr-link/apis";
 import { config } from "./config";
 import { SmsGatewayClient } from "./gateway";
-import { createClient } from "./queue";
 import { createLogger } from "./logger";
+import { createClient } from "./queue";
+import { SenderService } from "./services/sender";
 
 async function main() {
     const logger = createLogger();
     const queue = createClient(config.BROKER_URL);
     const gateway = new SmsGatewayClient(config.GATEWAY_URL);
 
+    const sendService = new SenderService(
+        config.SEND_QUEUE,
+        queue,
+        gateway,
+        logger
+    );
+
     await queue.start();
-
-    const unsubscribe = await queue.subscribe<SendMessage>(config.SEND_QUEUE, async (queueName, message) => {
-        const { error } = SendMessageSchema.validate(message);
-        if (error) {
-            logger.error("Invalid message:", error);
-            return;
-        }
-
-        if (message.channel !== NotificationChannel.Phone) {
-            logger.error("Unsupported channel", message);
-            return;
-        }
-
-        logger.info('Message received', message);
-
-        await gateway.send({
-            to: message.to,
-            text: message.message,
-        });
-
-        logger.info('Message sent', message);
-    });
-    logger.info(`Listening on ${config.SEND_QUEUE}...`);
+    await sendService.start();
 
     process.on('SIGINT', async () => {
         logger.info('Shutting down...');
-        unsubscribe();
+        await sendService.stop();
         await queue.close();
 
         logger.info('Bye!');
